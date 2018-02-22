157e3fe52e1babd09a5ad41853dc9327189db3fd
Le0Wolf <dev@safindanil.info>
2018-02-22 15:19:11 +0300

    First release

---
+++ WordGoogleTranslateAddin/GUI/TranslateButton.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new TranslateButton();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace WordGoogleTranslateAddin.GUI
+{
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    [ComVisible(true)]
+    public class TranslateButton : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Action OnTranslateButtonPressed { get; set; }
+
+        public TranslateButton()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("WordGoogleTranslateAddin.GUI.TranslateButton.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        public Bitmap GetImage(Office.IRibbonControl control)
+        {
+            switch (control.Id)
+            {
+                case "translateItButton":
+                    return new Bitmap(Properties.Resources.Google_Translate_Icon);
+            }
+
+            return null;
+        }
+
+        public void OnTranslatePressed(Office.IRibbonControl control)
+        {
+            switch (control.Id)
+            {
+                case "translateItButton":
+                    this.OnTranslateButtonPressed();
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ WordGoogleTranslateAddin/ThisAddIn.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace WordGoogleTranslateAddin
+{
+    using System.IO;
+    using System.Windows.Forms;
+
+    using Google.Apis.Auth.OAuth2;
+    using Google.Cloud.Translation.V2;
+
+    using WordGoogleTranslateAddin.GUI;
+
+    public partial class ThisAddIn
+    {
+        private TranslationClient translationClient;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            using (var key = new MemoryStream(Properties.Resources.GoogleCloudPrivateKey))
+            {
+                var credential = GoogleCredential.FromStream(key);
+                this.translationClient = TranslationClient.Create(credential, TranslationModel.NeuralMachineTranslation);
+            }
+        }
+
+        private void TranslateSelected()
+        {
+            var selected = this.Application.Selection;
+            if (selected.Type == Word.WdSelectionType.wdNoSelection)
+            {
+                return;
+            }
+
+            var text = selected.Text;
+            var formatedText = selected.FormattedText.WordOpenXML;
+
+            var translationResult = this.translationClient.TranslateText(selected.Text, "ru", "en");
+            selected.Text = translationResult.TranslatedText;
+
+            //var selectedText = Clipboard.GetData(DataFormats.Html);
+        }
+
+        protected override Office.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new TranslateButton
+            {
+                OnTranslateButtonPressed = this.TranslateSelected
+            };
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+        }
+        
+        #endregion
+    }
+}
